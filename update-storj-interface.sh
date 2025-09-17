b7229e410e4acd8814ffdb89ec94afab82b23baa
tushar-saxena-yral <tushar@gobazzinga.io>
2025-09-17 20:22:57 +0530

    feat: check for status when updating, and add ci

--- update-storj-interface.sh
+++ update-storj-interface.sh
@@ -9,4 +9,8 @@ echo "Installing the binary"
 install -m 755 /tmp/storj-interface /usr/local/bin/storj-interface
 
 echo "Requesting a restart with systemd"
-systemctl restart storj-interface
+systemctl restart storj-interface && \
+systemctl is-active --wait storj-interface && \
+echo "waiting for startup" && \
+sleep 10 && \
+systemctl is-active --quiet storj-interface
